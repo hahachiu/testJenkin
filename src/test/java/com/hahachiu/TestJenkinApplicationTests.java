efec1bff7fd04c21668d1095b68704f74f4ef8ce
hahachiu <fslyc528000@gmail.com>
2018-01-28 17:47:26 +0800

    test

--- src/test/java/com/hahachiu/TestJenkinApplicationTests.java
+++ src/test/java/com/hahachiu/TestJenkinApplicationTests.java
@@ -1,16 +1,16 @@
-package com.hahachiu;
-
-import org.junit.Test;
-import org.junit.runner.RunWith;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.junit4.SpringRunner;
-
-@RunWith(SpringRunner.class)
-@SpringBootTest
-public class TestJenkinApplicationTests {
-
-	@Test
-	public void contextLoads() {
-	}
-
-}
+//package com.hahachiu;
+//
+//import org.junit.Test;
+//import org.junit.runner.RunWith;
+//import org.springframework.boot.test.context.SpringBootTest;
+//import org.springframework.test.context.junit4.SpringRunner;
+//
+//@RunWith(SpringRunner.class)
+//@SpringBootTest
+//public class TestJenkinApplicationTests {
+//
+//	@Test
+//	public void contextLoads() {
+//	}
+//
+//}
